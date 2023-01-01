3e09795ebdf831a53b937cae30c9af7278dac926
zhang-ss1 <74695580+zhang-ss1@users.noreply.github.com>
2023-01-01 14:12:08 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -eu
 
 latest=v3.1.10
 #latest=$(curl -fsSL https://api.github.com/repos/cnk3x/xunlei/releases/latest | grep browser_download_url | grep $(uname -m) | head -n 1 | grep -Eo https.+.tar.gz)
-latest=https://gitee.com/zhang0510/ttnode/releases/download/v3.1.10/xunlei-v3.1.10.linux.armv8.tar.gz
+latest=https://gitee.com/zhang0510/ttnode/releases/download/v3.1.10/xunlei-v3.1.10.linux.aarch64.tar.gz
 echo "download: $latest"
 curl -fsSL ${latest} | tar zx
 ./xunlei $@
