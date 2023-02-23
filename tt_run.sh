a439fd68881fad9957ab9a9ec1408d5663a2525a
zhang-ss1 <74695580+zhang-ss1@users.noreply.github.com>
2023-02-23 12:57:51 +0800

    删除容器魔方相关命令
    因相关风险，删除容器魔方相关命令

--- tt_run.sh
+++ tt_run.sh
@@ -368,174 +368,6 @@ exit 0;
 }
 
 
-#部署网心容器魔方
-startWxedgeSever() {
-clear;
-echo "
-
-======================================================================================
-
-正在部署/更新网心容器魔方，请耐心等待
-
-======================================================================================
-
-";
-#添加开机自动执行脚本
-echo -e 'sleep 2s
-swapoff -a
-sleep 2s
-/usr/node/mount.sh
-sleep 5s
-if [[ "$(docker inspect wxedge 2> /dev/null | grep \x27"Name": "/wxedge"\x27)" != "" ]];
-then
-docker restart $(docker ps -q)
-else
-docker run \
---name=wxedge \
---restart=always \
---privileged \
---net=host \
---tmpfs /run \
---tmpfs /tmp \
--v /mnts/wxedge1/containerd:/var/lib/containerd \
--v /mnts/wxedge1:/storage:rw \
--d \
-registry.hub.docker.com/onething1/wxedge:latest
-fi' > /etc/local.d/mount.start
-chmod +x /etc/local.d/mount.start;
-sleep 1s;
-rc-update add local;
-
-
-docker run \
---name=wxedge \
---restart=always \
---privileged \
---net=host \
---tmpfs /run \
---tmpfs /tmp \
--v /mnts/wxedge1/containerd:/var/lib/containerd \
--v /mnts/wxedge1:/storage:rw \
--d \
-registry.hub.docker.com/onething1/wxedge:latest
-
-sleep 10s;
-echo "
-
-======================================================================================
-
-部署完成，请浏览器输入“ http://$(ip route get 1.2.3.4 | awk '{print $7}'):18888 ”进行二维码扫码绑定、业务选择等操作！！！
-
-======================================================================================
-
-";
-exit 0;
-}
-
-
-
-#删除容器魔方缓存
-deleteWxedgeTaskCache() {
-clear;
-choosedeletetask='ture';
-while [ $choosedeletetask == 'ture' ] ;do
-read -p "
-======================================================================================
-
- 请输入序号，删除对应业务的所有缓存
-
-	1.B网盘
-	2.CB*	
-	3.CG*
-	4.CX*
-	5.CY
-	6.CYK
-	7.Z
-	8.删除所有缓存
-	9.退出
-
-======================================================================================
-
-
-请选择相应的数字进行操作：" deletecache
-
-if [[ ${deletecache} == 1 ]];then
-sleep 2s;
-echo '删除缓存中...';
-sleep 2s;
-rm -rf /mnts/wxedge1/.onething_data/task/984017bcbfc37757a79ff41324d54008*;
-echo '删除缓存完成，正在返回选项列表';
-sleep 3s;
-
-elif [[ ${deletecache} == 2 ]];then
-sleep 2s;
-echo '删除缓存中...';
-sleep 2s;
-rm -rf /mnts/wxedge1/.onething_data/task/5b442b259766008357b638af7d07d18a*;
-echo '删除缓存完成，正在返回选项列表';
-sleep 3s;
-
-elif [[ ${deletecache} == 3 ]];then
-sleep 2s;
-echo '删除缓存中...';
-sleep 2s;
-rm -rf /mnts/wxedge1/.onething_data/task/bae5a079ee6d7ac69ea2ac8cec142662*;
-echo '删除缓存完成，正在返回选项列表';
-sleep 3s;
-
-elif [[ ${deletecache} == 4 ]];then
-sleep 2s;
-echo '删除缓存中...';
-sleep 2s;
-rm -rf /mnts/wxedge1/.onething_data/task/e9b6850481e687306e41eb10b26e70bd*;
-echo '删除缓存完成，正在返回选项列表';
-sleep 3s;
-
-elif [[ ${deletecache} == 5 ]];then
-sleep 2s;
-echo '删除缓存中...';
-sleep 2s;
-rm -rf /mnts/wxedge1/.onething_data/task/34341e4c1d629e9ea0336960a9f5d2a8*;
-echo '删除缓存完成，正在返回选项列表';
-sleep 3s;
-
-elif [[ ${deletecache} == 6 ]];then
-sleep 2s;
-echo '删除缓存中...';
-sleep 2s;
-rm -rf /mnts/wxedge1/.onething_data/task/a040bc3e8db50b750167b22cf3e908fa*;
-echo '删除缓存完成，正在返回选项列表';
-sleep 3s;
-
-elif [[ ${deletecache} == 7 ]];then
-sleep 2s;
-echo '删除缓存中...';
-sleep 2s;
-rm -rf /mnts/wxedge1/.onething_data/task/18e5e85d32df5a474f509aa86bcf0015*;
-echo '删除缓存完成，正在返回选项列表';
-sleep 3s;
-
-elif [[ ${deletecache} == 8 ]];then
-sleep 2s;
-echo '删除缓存中...';
-sleep 2s;
-rm -rf /mnts/wxedge1/.onething_data/task/*;
-echo '删除缓存完成，正在返回选项列表';
-sleep 3s;
-
-else
-echo "
-
-退出删除缓存
-";
-sleep 2s;
-choosedeletetask='false';
-fi
-done
-}
-
-
-
 #开始前的说明
 sleep 1s;
 clear;
@@ -544,21 +376,19 @@ while [ $choosetask == 'ture' ] ;do
 read -p "
 ======================================================================================
 更新时间：2022-12-05
-当前脚本只适用于  “X86的alpine系统”  安装甜糖服务/网心容器魔方，若选错了按Ctrl+C即可结束安装
+当前脚本只适用于  “X86的alpine系统”  安装甜糖服务，若选错了按Ctrl+C即可结束安装
  
   请输入下列序号，进行相应操作
 	
-	1.一键部署甜糖服务（开启高质量通道，内存≥2G，硬盘≥128G，带宽≥100Mbps）
-	2.一键部署甜糖服务（非高质量通道，硬盘≥32G）
-	3.一键部署网心容器魔方
+	1.一键部署docker甜糖服务（开启高质量通道，内存≥2G，硬盘≥128G，带宽≥100Mbps）
+	2.一键部署docker甜糖服务（非高质量通道，硬盘≥32G）
+	3.一键部署甜糖服务（二进制程序）
 	4.硬盘分区并格式化（更换缓存盘后使用）
 	5.清除甜糖缓存（不会删除绑定信息）
-	6.删除甜糖容器
-	7.更新甜糖容器（高质量通道）
-	8.更新甜糖容器（非质量通道）
-	9.清除容器魔方缓存(不删除绑定信息)
-	10.删除容器魔方容器
-	11.更新容器魔方容器
+	6.删除甜糖容器（docker）
+	7.更新甜糖容器（docker，高质量通道）
+	8.更新甜糖容器（docker，非质量通道）
+	9.删除甜糖服务（二进制程序）
 	12.退出
 
 ======================================================================================
@@ -608,24 +438,9 @@ sleep 2s;
 startTtnodesever
 
 elif [[ ${beforestart} == 3 ]];then
-sleep 1s;
-startEvn
-#初始化硬盘
-/usr/node/automkfs.sh;
-sleep 1s;
-#挂载硬盘
-echo "
-
-======================================================================================
-
-正在尝试挂载硬盘，请稍等。。。
-
-======================================================================================
-
+echo"
+暂未完工
 ";
-/usr/node/mount.sh;
-sleep 2s;
-startWxedgeSever
 
 elif [[ ${beforestart} == 4 ]];then
 sleep 1s;
@@ -688,29 +503,10 @@ docker rmi -f tiptime/ttnode-test:latest >/dev/null 2>&1 || echo 'remove tiptime
 sleep 1s;
 startTtnodesever
 
-
 elif [[ ${beforestart} == 9 ]];then
-sleep 1s;
-echo '正在停止容器魔方...';
-docker stop wxedge >/dev/null 2>&1 || echo '容器不存在，不影响删除缓存' 
-sleep 2s;
-echo '删除缓存中...';
-deleteWxedgeTaskCache;
-sleep 2s;
-echo '正在启动容器魔方...';
-docker start wxedge >/dev/null 2>&1 || echo '容器不存在，不影响删除缓存' 
-
-elif [[ ${beforestart} == 10 ]];then
-sleep 1s;
-rm -rf /etc/local.d/mount.start;
-docker rm -f wxedge >/dev/null 2>&1 || echo 'remove wxedge container'
-docker rmi -f registry.hub.docker.com/onething1/wxedge:latest >/dev/null 2>&1 || echo 'remove onething1/wxedge from dockerhub'
-
-elif [[ ${beforestart} == 11 ]];then
-sleep 1s;
-docker rm -f wxedge  >/dev/null 2>&1 || echo 'remove wxedge container'
-docker rmi -f registry.hub.docker.com/onething1/wxedge:latest  >/dev/null 2>&1 || echo 'remove onething1/wxedge from dockerhub'
-startWxedgeSever
+echo"
+暂未完工
+";
 
 else
 echo "
